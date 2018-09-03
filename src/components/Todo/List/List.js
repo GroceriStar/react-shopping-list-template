eff11ebeacf80ec5a0bf13366d6f27df4eb01030
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-09-04 01:20:18 +0300

    update

--- src/components/Todo/List/List.js
+++ src/components/Todo/List/List.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+
+// @TODO delete later and move to parent
+import data from '@groceristar/groceristar-fetch/groceristar';
 
 class List extends Component {
-    render() {
-        return (
-            <div>List component</div>
-        );
-    }
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+
+    const count = [1, 2, 3].length;
+    // const items = [1, 2, 3];
+    //data is things from gs-fetch
+    // const items = props.data;
+    const items = data
+      return (
+          <div>List component</div>
+      );
+  }
 }
 
 export default List;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,21 +2,40 @@ import React, {
   Component
 } from 'react';
 import classNames from 'classnames';
+import PropTypes  from 'prop-types';
 
 
 class Todo extends Component {
 
+  constructor(props){
+    super(props);
 
 
+  }
 
-    render() {
+  render() {
+    const handleToggle = this.props.handleToggle;
+    const handleRemove = this.props.handleRemove;
+    const elementClass = 'xxx';
+    return (
+      <li>
+        <span onClick={handleRemove}>Text</span>
+        <input
+          type="checkbox"
+          onChange={handleToggle}
+          checked={this.props.isComplete}
+          className={elementClass}
+        />
+        props.name
+      </li>
+    );
+  }
+}
 
-      return (
-            <div>
-              Todo component
-            </div>
-      );
-    }
+Todo.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  isComplete: React.PropTypes.bool,
+  id: React.PropTypes.number.isRequired
 }
 
 export default Todo;
