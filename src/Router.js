a948258254265314be822f9e037011cad849320c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-11-28 20:19:13 +0200

    Routes/ Grocery changes

--- src/Router.js
+++ src/Router.js
@@ -7,15 +7,23 @@ import {
 } from 'react-router-dom';
 
 
+import OldRouter from './OldRouter';
 
-import Home        from './views/Home/Home';
-import Profile     from './views/Profile/Profile';
-import New         from './views/New/New';
-import List        from './screens/List/List';
-import DisplayList from './screens/DisplayList/DisplayList';
-import Departments from './screens/Departments/Departments';
-import Ingredients from './screens/Ingredients/Ingredients';
-import Examples    from './views/Examples/Examples';
+
+// import GroceryRouter from ''
+// import DepartmentsRouter from ''
+// import IngredientsRouter from ''
+// import FavoritesRouter from ''
+// import PurchasedRouter from ''
+
+// import Home        from './views/Home/Home';
+// import Profile     from './views/Profile/Profile';
+// import New         from './views/New/New';
+// import List        from './screens/List/List';
+// import DisplayList from './screens/DisplayList/DisplayList';
+// import Departments from './screens/Departments/Departments';
+// import Ingredients from './screens/Ingredients/Ingredients';
+// import Examples    from './views/Examples/Examples';
 
 
 
@@ -26,16 +34,7 @@ class Router extends Component {
   render() {
     return (
           <Switch>
-              <Route path='/'            component={Home} exact />
-              <Route path='/profile'     component={Profile} />
-              <Route path='/new'         component={New} />
-              <Route path='/list'        component={List} />
-              <Route path='/departments' component={Departments} />
-              {/* Are we reallly can have exact for a few routes? */}
-              <Route path='/display'     component={DisplayList} exact />
-              <Route path='/ingredients' component={Ingredients} exact />
-
-              <Route path='/examples'    component={Examples} exact />
+              <Route path='/'            component={OldRouter} exact />
           </Switch>
     );
   }
---
+++ src/views/Groceries/ManageGroceryListView.js
@@ -0,0 +1,4 @@
+// This page giving you an ability to review departments and ingredients that we have
+// also you have option to delete department with all ingredients inside(later maybe we'll add a prompt are you really sure)
+// this view should receive a grocery list id in order to display all this data.
+// 
