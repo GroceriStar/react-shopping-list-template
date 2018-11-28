660378c03ded57e05f00a95d3797846c93e24ecb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-11-28 20:03:54 +0200

    layouts

---
+++ src/layouts/ProfileLayout.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+class ProfileLayout extends Component {
+    render() {
+        return (
+          <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+        );
+    }
+}
+
+export default ProfileLayout;
