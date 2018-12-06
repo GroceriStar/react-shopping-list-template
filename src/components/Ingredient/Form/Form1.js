fbfa4fae628de6164a5ee90138048095c61a514e
Vadim <35640573+vadim9999@users.noreply.github.com>
2018-12-06 18:56:08 +0200

    Add Form1

---
+++ src/components/Ingredient/Form/Form1.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Form1 extends Component {
+  render() {
+    return (
+      <div>
+        <label>Input Ingredient name</label>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default Form1;
