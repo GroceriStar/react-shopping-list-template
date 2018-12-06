40fe0897e125e930b146580765b644143433c365
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-07 01:42:15 +0200

    Create selector2.js

---
+++ src/selectors/selector2.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const serverURL = '';
+
+let GroceristarAPI = {};
+
+GroceristarAPI.methodA = function() => {
+
+  console.log(serverURL)
+
+};
+
+export default GroceristarAPI;
