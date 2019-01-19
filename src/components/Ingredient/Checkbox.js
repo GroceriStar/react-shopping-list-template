f396d1dbcd336bf700b64f8440c056bb749c58c8
Vadim <vadim.putrov@gmail.com>
2019-01-19 18:24:20 +0200

    add decoration to ckeckbox

--- src/components/Ingredient/Checkbox.js
+++ src/components/Ingredient/Checkbox.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
 class Checkbox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      checked : false,
+      style:{
+        'text-decoration': 'none'
+      }
+    }
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(){
+    console.log("checked");
+    this.setState({
+      checked: !this.state.checked,
+      style:{
+        'text-decoration':
+         !this.state.checked ? 'line-through' : 'none',
+       },
+
+         })
+  }
+
     render() {
         return (
-              <label>
-            <input type="checkbox" name={this.props.name} />
-            {this.props.children}
-            </label>
+          <label style={this.state.style}>
+            <input type="checkbox" name={this.props.name} onClick={this.handleChange} />
+              {this.props.children}
+          </label>
         );
     }
 }
